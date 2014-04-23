6bcc6599583cb4b802e4725e3b094b22cca3675b
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2014-04-23 10:42:27 +0900

    import opencv-ar and enable to compile by cmake

---
+++ include/opencvar/acgl.h
@@ -0,0 +1,200 @@
+/*************
+ Copyright (c) 2011, Allen Choong
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the project nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************/
+
+/**
+ * \file acgl.h
+ * \brief Header acgl
+ *
+ * Description: Several OpenGL functions, for easier use.
+ * 20100226 Add acGlSelectd() because of OpenSceneGraph
+ * \author	Allen
+ * \date	20080904
+ * \version	0.1.0
+ *
+ * \deprecated acGlSelect() 
+ * Superceded by acGlSelect2()
+ */
+
+#ifndef AC_GL_H
+#define AC_GL_H
+
+#ifdef __MINGW32__
+#	include <GL/glext.h>
+#endif
+
+#ifdef ACDLL
+#	define AC_DLL __declspec(dllexport)
+#else
+#	define AC_DLL
+#endif
+
+/**
+ * \defgroup functionGl acgl functions
+ * @{
+ */
+
+extern "C" {
+/**
+ * \brief Create image texture mapping
+ *
+ * For the video, with the ratio height : width = 1: width/height
+ * \param depth	How many bytes per pixel
+ * \param swap	1 for swapping RGB, 0 no swapping needed.
+ */
+AC_DLL void acGlTexture(unsigned char* data, int width, int height, int depth, int swap = 0);
+
+/**
+ * The definition of this function is written again and again. Therefore, just make it
+ * into a function. The disadvantage of this function is generate the texture and delete
+ * the texture every time render.
+ * This function is used for video (or image), despise the ratio of width and height
+ * Therefore, it is very good in GL_PROJECTION.
+ * The function is specifically to map the whole window with the texture. Therefore,
+ * remember to use GL_TEXTURE_2D
+ */
+AC_DLL void acGlTextureProject(void* buffer, int width, int height, int depth, int swap = 0);
+
+/**
+ * \brief Selection/picking
+ *
+ * For picking or selection, the projection and modelview are defined outside the
+ * function, so that, it is able to be used in custom environment <br>
+ * Note that, glLoadName() is needed in the draw() callback function so that the value
+ * of the "name" will be returned.
+ * \param x	Coordinate-x
+ * \param y Coordinate-y
+ * \param projection	OpenGL projection matrix
+ * \param modelview	OpenGL modelview matrix
+ * \param draw	Callback function to draw the selectable/pickable object
+ * \return Return the "name" of the object, if no object selected, return 0.
+ */
+AC_DLL int acGlSelect(int x, int y, float* projection, float* modelview, void (*draw)());
+
+/**
+ * Not sure it will work in all windows or not
+ */
+AC_DLL void acGlGetWindowSize(int* width, int* height);
+
+/**
+ * Get the ratio of "raster float" to "window pixel"
+ */
+AC_DLL float acGlPixelRasterRatioX();
+AC_DLL float acGlPixelRasterRatioY();
+
+/**
+ * Super power conversion function. Actually, the pixel refers to the 3D transformation unit. Not the windows 2D pixel unit.
+ * This is because, the pixels will be influenced by the 3D transformation.
+ * Note: These functions should not be used after glRasterPos(), cause it will reset the state
+ */
+AC_DLL float acGlPixel2RasterX(float input); //For raster, convert the value of glGet() to glRasterPos()
+AC_DLL float acGlRaster2PixelX(float input);
+AC_DLL float acGlPixel2RasterY(float input);
+AC_DLL float acGlRaster2PixelY(float input);
+
+/**
+ * GLUT font functions
+ */
+AC_DLL void acGlutFontBitmap(char* str);
+AC_DLL void acGlutFontStroke(char* str);
+
+/**
+ * Print OpenGL state
+ */
+AC_DLL void acGlPrintState();
+
+/**
+ * Print OpenGL glGet*()
+ */
+AC_DLL void acGlPrintGet(GLenum pname);
+
+/**
+ * \brief Default process hit function
+ * \return Front object
+ */
+AC_DLL int acGlProcessHit(GLint hits, GLuint buffer[]);
+
+/**
+ * \brief Select OpenGL matrix
+ * \param processHit	Callback function
+ * \return If nothing, return -1
+ */
+AC_DLL int acGlSelect2(int x, int y, int width, int height, float* projection, float* modelview,
+                       void (*draw)(),
+                       int (*processHit)(GLint hit, GLuint buffer[]) = acGlProcessHit);
+
+/**
+ * \brief Select OpenGL matrix in double
+ * \param processHit	Callback function
+ * \param viewport	If want get self-defined viewport
+ * \param inverse	Inverse the y-coordinate
+ * \return If nothing, return -1
+ */
+AC_DLL int acGlSelectd(int x, int y, int width, int height, double* projection, double* modelview,
+                       void (*draw)(), GLint viewport[4] = 0, int inverse = 0,
+                       int (*processHit)(GLint hit, GLuint buffer[]) = acGlProcessHit);
+
+/**
+ * \brief Check object (point) is occluded or not
+ * \return 0 if no occlusion, else 1.
+ */
+AC_DLL int acGlIsOccluded(float objX, float objY, float objZ);
+
+} //extern
+
+/** @} end group */
+
+/**************
+ Object font
+ **************/
+
+/**
+ * \brief WGL bitmap font
+ */
+class AC_DLL acWglFont {
+public:
+    acWglFont();
+    virtual ~acWglFont();
+
+    /**
+     * \brief Initialization
+     */
+    void init(char* szWindow, char* szTypeface, int size = 12, int bold = 0, int italic = 0,
+              int underline = 0, int strikeout = 0);
+
+    void print(const char* str, ...);
+    void printMulti(int height, const char* str, ...);
+
+    int isInit();
+    void destroy();
+
+private:
+    char* m_szWindow;
+    GLuint m_uBase;
+    int m_bInit;
+};
+
+#endif
---
+++ include/opencvar/acmath.h
@@ -0,0 +1,230 @@
+/*************
+ Copyright (c) 2011, Allen Choong
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the project nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************/
+
+/**
+ * \file acmath.h
+ * \brief Math module
+ *
+ * These are the old functions that I used. The primary purpose is for 3D computer graphic,
+ * thus, the vector is 3, matrix is 4x4, and also based on OpenGL, column-major matrix.
+ * \author Allen
+ * \date 20090507
+ * \todo Decompose the transformation matrix.
+ * Add in 3x3 matrix inversion, 2x2.
+ */
+
+#ifndef AC_MATH_H
+#define AC_MATH_H
+
+#ifdef ACDLL
+#	define AC_DLL __declspec(dllexport)
+#else
+#	define AC_DLL
+#endif
+
+/**
+ * This struct follows OpenCV point structure. So that, without OpenCV,
+ * it is also possible to use my own strucutre
+ */
+struct AcPointi {
+    int x;
+    int y;
+};
+
+/**
+ * Same as above
+ */
+struct AcPointf {
+    float x;
+    float y;
+};
+
+extern "C" {
+
+/**
+ * \defgroup mathFunc	Math module
+ *
+ * Related to math
+ * @{
+ */
+
+AC_DLL void acVectorPrint(float *v);
+AC_DLL void acVectorAdd(float *v1, float *v2, float *vOut);
+AC_DLL void acVectorDeduct(float *v1, float *v2, float *vOut);
+AC_DLL void acVectorCrossProduct(float *v1, float *v2, float *product);
+
+/**
+ * Description:
+ * v1, v2, and v3, should be vertices, with x,y,z for each vertex.
+ * They are input value.
+ * "normal" will be the output value. It must be a vector with x,y,z also
+ */
+AC_DLL void acVectorNormal(float *v1, float *v2, float *v3, float *normal);
+AC_DLL float acVectorMagnitude(float *v);
+AC_DLL void acVectorNormalise(float *vIn, float *vOut);
+
+/**
+ * Get the normalised normal
+ */
+AC_DLL void acVectorNormal2(float *v1, float *v2, float *v3, float *nv);
+
+AC_DLL float acRad2Deg(float rad);
+AC_DLL float acDeg2Rad(float deg);
+
+/**
+ * This coding is according to the calculation of OpenGL, therefore the output matrix is also 
+ * according to OpenGL matrix
+ */
+AC_DLL void acMatrixRotate(float deg, float x, float y, float z, float *m);
+AC_DLL void acMatrixTranslate(float x, float y, float z, float *m);
+AC_DLL void acMatrixScale(float x, float y, float z, float *m);
+AC_DLL void acMatrixIdentity(float *m);
+
+/**
+ * Dot product base on row-majored matrix
+ * Therefore, m1 and m2 should be row-majored matrix
+ */
+AC_DLL float acMatrixDotProduct(float *m1, float *m2, int col, int row);
+
+/**
+ * Based on row-majored matrix
+ * m1, m2 and mOut should be row-majored
+ */
+AC_DLL void acMatrixMultiply(float *m1, float *m2, float *mOut);
+AC_DLL void acMatrixPrint(float *m);
+AC_DLL void acMatrixTranspose(float *m);
+
+/**
+ * Transpose matrix in double type
+ */
+AC_DLL void acMatrixTransposed(double* m);
+
+/**
+ * The algorithm come from http://www.j3d.org/matrix_faq/matrfaq_latest.html
+ * \param q Quaternion with w,x,y,z
+ */
+AC_DLL void acMatrixToQuaternion(double* m, double* q);
+
+/**
+ * Convert quaternion to matrix in 4x4, the data of the matrix will be
+ * overwritten.
+ */
+AC_DLL void acQuaternionToMatrix(double* q, double* m);
+
+/**
+ * This algorithm is from OpenCV square example
+ */
+AC_DLL double acAngle(AcPointi* pt1, AcPointi* pt2, AcPointi* pt0);
+
+/**
+ * Calculate length between two vertices
+ */
+AC_DLL double acCalcLength(AcPointf pt1, AcPointf pt2);
+
+/**
+ * \brief Invert modelview matrix 4x4
+ *
+ * From http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
+ */
+AC_DLL void acMatrix4Invert(float m[]);
+
+/**
+ * \brief Get determinant of 4x4 matrix
+ *
+ ** From http://www.euclideanspace.com/maths/algebra/matrix/functions/inverse/fourD/index.htm
+ */
+AC_DLL float acMatrix4GetDeterminant(float m[]);
+
+/**
+ * \brief decompose the 4x4 matrix
+ *
+ * Algorithm from http://www.gamedev.net/community/forums/topic.asp?topic_id=441695
+ * \param m	[in] 4x4 row-majored transformation matrix
+ * \param t	[out]	Translation 3x1 vector
+ * \param s	[out] Scale 3x1 vector
+ * \param r	[out] Rotation 4x4 matrix
+ */
+AC_DLL void acMatrixDecompose(float m[], float t[], float s[], float r[]);
+
+/************
+ Bit operation and 2D square array, related to image processing
+ ***********/
+
+/**
+ * \brief Rotate square array in unsigned byte
+ * \param rot 1 = 90 deg clockwise, 2 = 180 deg clockwise, 3 = 270 deg clockwise
+ */
+AC_DLL void acArray2DRotateub(unsigned char* arr, int w, int h, int rot);
+
+/**
+ * \brief Print array 2D
+ */
+AC_DLL void acArray2DPrintub(unsigned char* arr, int w, int h);
+
+/**
+ * \brief Convert array 2D to bit
+ *
+ * The array data must be binary. The bit data is like the following feature <br>
+ * Array sequence:
+ * 1248 1248 (bit)
+ * 0000 0000 => 0 0 <br>
+ * 0001 1000 => 8 1<br>
+ * 0011 1100 => C 3<br>
+ * 0111 1110 => E 7<br>
+ * 1000 0001 => 1 8<br>
+ * 0100 0010 => 2 4<br>
+ * 0010 0100 => 4 2<br>
+ * Result: 0x00183C7E814224 <br>
+ * And remember that, the image should be flipped vertically, that means printing the array will always
+ * flipped from the image, because the printing is print bottom-up. <br>
+ * If read the hex from left to right, then, the image should be read from right to left yet bottom-up, and vice versa. <br>
+ * Thus, if a person want to convert the image code to hex manually, write the hex from left to right, then he/she need to read
+ * the image from right to left and bottom-up.
+ * \param bit	Store the output, the size of bit must be 
+ * large enough to hold the data, such as "long long" for 8-byte (or 64bit)
+ */
+AC_DLL void acArray2DToBit(unsigned char* arr, int w, int h, long long int* bit);
+
+/**
+ * \brief Convert bit to 2D array
+ */
+AC_DLL void acBitToArray2D(long long int bit, unsigned char* arr, int w, int h);
+
+/**
+ * \brief Rotate bit
+ *
+ * Convert the bit to 2D square array, then rotate, then convert to bit again
+ * Expected size of bit is 64-bit.
+ * \param rot 1=90 degree clockwise, 2=180, 3=270
+ */
+AC_DLL void acBitRotate(long long int* bit, int rot);
+
+/** @} end mathFunc */
+
+} //extern
+
+#endif
